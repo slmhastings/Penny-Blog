a4bb92db26766b97c1d54ee47a771b9b27c9ea43
Sequona Hastings <slmhastings@gmail.com>
2021-03-25 18:00:13 -0500

    add logo to browser

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -23,13 +23,13 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          href={`${withPrefix('/')}img/browser-icon-32x32.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          href={`${withPrefix('/')}img/browser-icon-16x16.png`}
           sizes="16x16"
         />
 
