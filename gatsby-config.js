4b7bce3736bb360653a27a571dbd55ece404cdff
Sequona Hastings <slmhastings@gmail.com>
2021-04-14 11:13:17 -0500

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,17 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-7R28Z0YR0N", // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
+    {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
