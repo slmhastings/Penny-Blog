b58b0f2723f3ea6709e10e1181e5effc6571271b
Sequona Hastings <slmhastings@gmail.com>
2021-04-17 18:20:43 -0500

    fix author footer

--- src/components/Authorfooter.js
+++ src/components/Authorfooter.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import authorfooter from '../../static/img/authorfooter.png'
+
 
 
 function Authorfooter() {
   return (
     <div class="author-footer">
       <div>
+        <div className="author-bio" style={{
+          fontFamily: "Harmattan"
+        }}>
+          <p>Written by Crissi Cole and Rebeca Menini.</p>
+          <p>Crissi is the founder and CEO of Penny Finance. Rebeca is lead copywriter for Penny Finance.</p>
+          <p>Penny is the first personalized, tech-powered financial mentor that provides real-world financial education</p>
+          <p>and know-how to women at the starting line of their financial lives.
+          </p>
+        </div>
         <img src="/img/authorfooter-mobile.svg" style={{
           width: "74vh"
         }}></img>
