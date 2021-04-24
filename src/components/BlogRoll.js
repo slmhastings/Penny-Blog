833e0c60d7c2e70fe6032e741b9bb295ee5e6d51
Sequona Hastings <slmhastings@gmail.com>
2021-04-24 18:09:37 -0500

    change media size

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -10,7 +10,9 @@ class BlogRoll extends React.Component {
     const { markdownRemark: post } = data
 
     return (
-      <div className="columns is-multiline" >
+      <div className="columns is-multiline" style={{
+        
+      }}>
         {posts &&
           posts.map(({ node: post }) => (
             <div className="is-parent column is-4" key={post.id}>
